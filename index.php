c913ea734d17cfa7dd5c86f8d44881b7fab7536a
ekpotosammuel <ekpotosammuel1@gmail.com>
2023-06-26 11:39:50 +0100

    clean dummy comment on app

--- index.php
+++ index.php
@@ -2,9 +2,7 @@
 
 $report = '';
 if (isset($_POST['submit'])) {
-    // echo 'button clicked';
     if ($_POST['lat'] == '' and $_POST['lon'] == '') {
-        // echo 'fill all filed';
     } else {
 
         $lat = $_POST['lat'];
@@ -14,11 +12,10 @@ if (isset($_POST['submit'])) {
         $data = file_get_contents("https://api.openweathermap.org/data/2.5/weather?lat=$lat&lon=$lon&appid=$key");
         // print_r($data);
         $weather_data = json_decode($data, true);
-        // print_r($weather_data);
-        // echo $weather_data['main']['temp'];
+
         // convert temperature to kelvin 
         $temp = $weather_data['main']['temp'] - 273;
-        // echo $temp;
+
         $report = "Today's weather is " . $temp . "in " . $weather_data['sys']['country'] . " with wind speed of " . $weather_data['wind']['speed'];
     }
 }
@@ -37,14 +34,11 @@ if (isset($_POST['submit'])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Weather</title>
-    <!-- <link rel="stylesheet" href="../dist/output.css"> -->
-    <!-- <link href="/dist/output.css" rel="stylesheet"> -->
+
 </head>
 
 <body>
-    <!-- <h1 class="text-3xl font-bold underline">
-        Hello world!
-    </h1> -->
+
     <form action="index.php" method="POST">
         <div>
             <h2 class="">weather</h2>
